a7f866f3d2d5026cd632860e83c90110d98ecd23
Stephane Talbot <Stephane.Talbot@univ-savoie.fr>
2023-03-12 19:55:22 +0100

    add functional tests

--- src/test/groovy/org/xbib/gradle/plugin/test/JFlexPluginTest.groovy
+++ src/test/groovy/org/xbib/gradle/plugin/test/JFlexPluginTest.groovy
@@ -81,6 +81,82 @@ jflex {
         }
     }
 
+    @Test
+    void testJFlexTaskWithCustomTarget() {
+        String settingsFileContent = '''
+rootProject.name = 'jflex-test'
+'''
+        settingsFile.write(settingsFileContent)
+        String buildFileContent = '''
+plugins {
+    id 'org.xbib.gradle.plugin.jflex'
+}
+
+sourceSets {
+  test {
+     jflex {
+       // point to our test directory where the jflex file lives
+       srcDir "${System.getProperty('user.dir')}/src/test/jflex"
+     }
+  }
+}
+
+jflex {
+   verbose = true
+   dump = false
+   progress = false
+}
+
+tasks.named('generateTestJflex').configure {
+   // write output into custom location .
+   target = file("${project.buildDir}/generated/sources/jflexTestCustom/")
+}
+
+'''
+        buildFile.write(buildFileContent)
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withArguments(":build", "--info")
+                .withPluginClasspath()
+                .forwardOutput()
+                .build()
+        assertEquals(TaskOutcome.SUCCESS, result.task(":build").getOutcome())
+        // default output dir
+        File targetJflex = new File(projectDir, "build/generated/sources/jflexTestCustom")
+        boolean found = false
+        if (targetJflex.exists()) {
+            // check for generated output
+            assertEquals(1, targetJflex.listFiles().length)
+            targetJflex.eachFileRecurse {
+                if (it.isFile()) {
+                    println "found: ${it}"
+                    found = true
+                }
+            }
+        } else {
+            fail("directory not found: ${targetJflex}")
+        }
+        if (!found) {
+            fail("jflex output not found")
+        }
+        File targetJava = new File(projectDir, "build/classes/java/test")
+        found = false
+        if (targetJava.exists()) {
+            // check for generated output
+            assertEquals(1, targetJava.listFiles().length)
+            targetJava.eachFileRecurse {
+                if (it.isFile()) {
+                    println "found: ${it}"
+                    found = true
+                }
+            }
+        } else {
+            fail("class directory not found: ${targetJava}")
+        }
+        if (!found) {
+            fail("java output not found")
+        }
+    }
 
     @Test
     void testJFlexWriteIntoJavaSrc() {
@@ -116,6 +192,76 @@ jflex {
    verbose = true
    dump = false
    progress = false
+   // enable legacy behavior of writing directly into Java source directory. Not recommended.
+   writeIntoJavaSrc = true
+}
+
+'''
+        buildFile.write(buildFileContent)
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withArguments(":build", "--info")
+                .withPluginClasspath()
+                .forwardOutput()
+                .build()
+        assertEquals(TaskOutcome.SUCCESS, result.task(":build").getOutcome())
+        // search the Java source directory
+        File target = new File(projectDir, "src/main/java")
+        boolean found = false
+        if (target.exists()) {
+            // check for generated file
+            assertEquals(1, target.listFiles().length)
+            target.eachFileRecurse {
+                if (it.isFile()) {
+                    println "found: ${it}"
+                    found = true
+                }
+            }
+        } else {
+            fail("directory not found: ${target}")
+        }
+        if (!found) {
+            fail("jflex output not found")
+        }
+    }
+
+    @Test
+    void testJFlexTaskWriteIntoJavaSrc() {
+        Files.createDirectories(projectDir.toPath().resolve('src/main/java'))
+        Files.createDirectories(projectDir.toPath().resolve('src/test/java'))
+        String settingsFileContent = '''
+rootProject.name = 'jflex-test'
+'''
+        settingsFile.write(settingsFileContent)
+        String buildFileContent = '''
+plugins {
+    id 'org.xbib.gradle.plugin.jflex'
+}
+
+sourceSets {
+  main {
+     java {
+       srcDir "src/main/java"
+     }
+     jflex {
+       // point to our test directory where the jflex file lives
+       srcDir "${System.getProperty('user.dir')}/src/test/jflex"
+     }
+  }
+  test {
+     java {
+       srcDir "src/test/java"
+     }
+  }
+}
+
+jflex {
+   verbose = true
+   dump = false
+   progress = false
+}
+
+tasks.named('generateJflex').configure {
    // enable legacy behavior of writing directly into Java source directory. Not recommended.
    writeIntoJavaSrc = true
 }
